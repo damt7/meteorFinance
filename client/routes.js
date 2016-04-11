ff13e062f6073d52f1b7746f5ac54942d1c875dc
damt7 <Ultra100>
2016-04-11 17:37:21 -0500

    Formulario de ingresos y egresos

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,16 @@ Router.configure({
 });
 
 Router.route('/', {name: 'home', controller: 'MainController'});
-Router.route('/ingresos', {name: 'ingresos', controller: 'MainController'});
+Router.route('/income', {name: 'income', controller: 'incomeController'});
 
 MainController = RouteController.extend({
   action: function() {
   	this.render('home', {});
   }
+});
+
+incomeController = RouteController.extend({
+    action: function() {
+        this.render('income', {});
+    }
 });
